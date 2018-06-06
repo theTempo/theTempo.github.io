8024bcd14a4493f91fc597eddd43fc21e2e6621d
theTempo <39846148+theTempo@users.noreply.github.com>
2018-06-06 09:36:30 -0700

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-$('#view-work').on('click', function() {
-    const images = $('#images').position().top;
-  
-    $('html, body').animate(
-      {
-      scrollTop: images
-      }, 
-    900)
-  });
